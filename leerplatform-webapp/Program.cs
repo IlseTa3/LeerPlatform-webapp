8898f4ce4f6d1e2c6092fb5e970b05509028f806
Ilse Tastenhoye <ilse.tastenhoye@gmail.com>
2024-10-02 19:41:08 +0200

    Fase 1: opzet project + verbinding db

--- leerplatform-webapp/Program.cs
+++ leerplatform-webapp/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure MySQL connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 36))));
 
 var app = builder.Build();
 
